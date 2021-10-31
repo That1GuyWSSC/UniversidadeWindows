0133de0c8419527af1c7180498d15d119160bcfb
Joao <a21151@alunos.ipca.pt>
2021-10-31 18:05:48 +0000

    read me

--- 2 ano/labinfo/email.ps1
+++ 2 ano/labinfo/email.ps1
@@ -1,4 +1,9 @@
-Copy-Item -Filter *. C:\Users\MoonShadow\Documents\teste D:\ 
+$Start= 'C:\Users\MoonShadow\Documents\teste'
+$End='D:\'
+
+copy-item -Path  $Start -destination $End  -Recurse 
+
+################################################
 
 $MailFrom='that1guywssc@gmail.com'
 $MailTo='joao.diogo.machado@gmail.com'
@@ -7,4 +12,4 @@ $Body="Mensagem que pretendemos mandar"
 $Subject='Mensagem do assunto do mail'
 
 
-Send-MailMessage -From "$MailFrom" -To "$MailTo" -SmtpServer "$SmtpServer" -Body "$Body" -Subject "$Subject" -Credential (Get-Credential) -UseSsl
+Send-MailMessage -From "$MailFrom" -To "$MailTo" -SmtpServer "$SmtpServer" -Body "$Body" -Subject "$Subject" -Credential (Get-Credential) -UseSsl  #-Attachments 
