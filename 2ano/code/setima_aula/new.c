54a8f1bed54965ebe631ada104595e44d857e1f8
that1guywssc <that1guywssc@gmail.com>
2021-12-03 18:42:34 +0000

    organizacao das pastas de labinfo

---
+++ 2ano/code/setima_aula/new.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void main(){
+
+    float total, compra, compras=0;
+    int quantidade= 0;
+
+    do
+    {
+        printf("Montante? : \n");
+        scanf("%f", &total);
+    } while (total <=0);
+
+    do
+    {   printf("Valor da compra %d : ", quantidade);
+        scanf("%f", &compra);
+        if (total >=compra)
+        {
+            total= total - compra;
+            quantidade++;
+            compras= compras= compra;
+            printf("Compra registrada! ");
+        }
+        
+    } while (total >=compra);
+    
+    printf("Saldo disponivel : %.2f euros \n", total);
+    printf("Quantidade : %d", quantidade);
+    
+
+}
