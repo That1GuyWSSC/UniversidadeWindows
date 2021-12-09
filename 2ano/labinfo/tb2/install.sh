2ba10dd2c0e9bbefde72bc20307e9b7d8b4c8842
that1guywssc <that1guywssc@gmail.com>
2021-12-09 09:12:08 +0000

    trabalho da aula de calculo antes do feriado

--- 2ano/labinfo/tb2/install.sh
+++ 2ano/labinfo/tb2/install.sh
@@ -34,6 +34,41 @@ Joseph Ragno 	... 	Ernie
 Jude Ciccolella 	... 	Guard Mert
 Paul McCrane 	... 	Guard Trout" >> castinfo
 
+
+echo " 
+
+Release date
+    October 14, 1994 (United States)
+
+Country of origin
+
+    United States
+
+Language
+
+    English
+
+Filming locations
+
+    127A Smithfield Road, Frederiksted, Virgin Islands
+
+Production company
+
+    Castle Rock Entertainment
+
+Box office
+
+    Budget
+        $25,000,000 (estimated)
+    Gross US & Canada
+        $28,699,976
+    Opening weekend US & Canada
+        $727,327Sep 25, 1994
+    Gross worldwide
+        $28,817,291
+
+                                " >> detalhes
+
 #Fim De Shawshank
 
 #Inicio da parte do The Godfather
@@ -52,6 +87,9 @@ echo "    Marlon Brando (Don Vito Corleone)
     Abe Vigoda (Tessio)
 
 										" >> castinfo
+
+
+echo ""
 
 
 #Fim da parte do GodFather
@@ -79,11 +117,171 @@ echo "    Al Pacino (Michael Corleone)
 cd ..
 mkdir The_Dark_Knight
 cd The_Dark_Knight
-echo ""
+echo "Christian Bale    ...     Bruce Wayne
+Heath Ledger    ...     Joker
+Aaron Eckhart   ...     Harvey Dent
+Michael Caine   ...     Alfred
+Maggie Gyllenhaal   ...     Rachel
+Gary Oldman     ...     Gordon
+Morgan Freeman  ...     Lucius Fox
+Monique Gabriela Curnen     ...     Ramirez
+Ron Dean    ...     Wuertz
+Cillian Murphy  ...     Scarecrow
+Chin Han    ...     Lau
+Nestor Carbonell    ...     Mayor
+Eric Roberts    ...     Maroni
+Ritchie Coster  ...     Chechen
+Anthony Michael Hall    ...     Engel
+Keith Szarabajka    ...     Stephens" >> castinfo
+
+
+# comeco da parte de 12 angry men
+
+cd ..
+mkdir 12_Angry_Men
+cd 12_Angry_Men
+echo "    Henry Fonda (Davis/Juror #8)
+    Martin Balsam (Juror #1)
+    John Fiedler (Juror #2)
+    Lee J. Cobb (Juror #3)
+    E.G. Marshall (Juror #4)
+    Jack Klugman (Juror #5)
+    Ed Begley (Juror #10)
+
+" >> castinfo
+
+#comeco da parte de Schindler's List (1993)
+
+cd ..
+mkdir Schindlers_List
+cd Schindlers_List
+echo "
+Liam Neeson     Oskar Schindler
+Ralph Fiennes   Amon Goeth
+Andrzej Seweryn     Julian Scherner
+Béatrice Macola     Ingrid
+Ben Kingsley    Itzhak Stern
+Caroline Goodall    Emilie Schindler
+Embeth Davidtz  Helen Hirsch
+Friedrich von Thun  Rolf Czurda
+Harry Nehring   Leo John
+Jonathan Sagall     Poldek Pfefferberg
+Krzysztof Luft  Herman Toffel
+Malgoscha Gebel     Wiktoria Klonowska
+Mark Ivanir     Marcel Goldberg
+Norbert Weisser     Albert Hujar
+Shmuel Levy     Wilek Chilowicz " >> castinfo
+
+
+#comeco da parte de Lord of the rings return of the king
+
+
+cd ..
+mkdir Lord_of_the_rings_Return_of_the_King
+cd Lord_of_the_rings_Return_of_the_King
+echo "Elijah Wood    Frodo Baggins 
+Ian McKellen    Gandalf 
+Viggo Mortensen     Aragorn 
+Sean Astin      Samwise Gamgee 
+Orlando Bloom   Legolas 
+Andy Serkis     Gollum/Sméagol 
+Billy Boyd      Pippin 
+Dominic Monaghan    Merry 
+John Rhys-Davies    Gimli 
+Liv Tyler   Arwen 
+Bernard Hill    King Théoden 
+Miranda Otto    Éowyn 
+Hugo Weaving    Elrond 
+Cate Blanchett  Galadriel 
+David Wenham    Faramir 
+Karl Urban  Éomer 
+John Noble  Denethor 
+                    " >> castinfo
+
+
+# comeco da parte de Pulp fiction
+
+cd ..
+mkdir Pulp_Fiction
+cd Pulp_Fiction
+echo "  
+John Travolta 
+ 
+Bruce Willis 
+ 
+Samuel L. Jackson 
+
+Uma Thurman 
+ 
+Christopher Walken 
+
+Ving Rhames Ca
+
+Tim Roth 
+ 
+Amanda Plummer 
+ 
+Harvey Keitel 
+
+Eric Stoltz 
+ 
+Maria de Medeiros
+
+Rosanna Arquette 
+ 
+Peter Greene 
+
+Frank Whaley 
+
+Burr Steers 
+
+Duane Whitaker 
+"  >> castinfo
 
 
 
+#comeco da parte de the good the bad and the ugly
 
+cd ..
+mkdir The_Good_The_Bad_and_The_Ugly
+cd The_Good_The_Bad_and_The_Ugly
+echo "
+Clint Eastwood 
+Lee Van Cleef 
+Eli Wallach 
+Aldo Giuffrè 
+Chelo Alonso 
+Luigi Pistilli 
+Antonio Molino Rojo 
+Enzo Petito 
+Rada Rassimov 
+Claudio Scarchilli 
+John Bartha 
+Livio Lorenzon 
+Antonio Casale 
+Sandro Scarchilli 
+Benito Stefanelli 
+Angelo Novi 
+                        " >> castinfo
+
+
+
+# comeco da parte de The Lord of the Rings: The Fellowship of the Ring
+
+cd ..
+mkdir The_Lord_of_the_Rings_The_Fellowship_of_the_Ring
+cd The_Lord_of_the_Rings_The_Fellowship_of_the_Ring
+echo "Frodo Baggins Elijah Wood
+Gandalf the Grey    Ian McKellen
+Arwen Undómiel  Liv Tyler
+Aragorn / Strider   Viggo Mortensen
+Samwise  Gamgee    Sean Astin
+Galadriel   Cate Blanchett
+Elrond  Hugo Weaving
+Boromir Sean Bean
+Bilbo Baggins   Ian Holm
+Saruman the White   Christopher Lee
+Gimli   John Rhys-Davies" >> castinfo
 
 
 
@@ -91,10 +289,11 @@ echo ""
 
 
 #Apagar a video todo os ficheiros da videoteca incluinda a propria diretoria
-cd ..
-cd ..
+cd $HOME/videoteca
 touch apagar.sh
 echo "#!/bin/bash
       cd ..
       rm -rf videoteca/" >> apagar.sh
       chmod +x apagar.sh
+
+zip -r videoteca.zip videoteca/
