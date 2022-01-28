5f20c7a6bd3ce7486d48e57ce6baab26e9bc1bd2
That1GuyWSSC <33987876+That1GuyWSSC@users.noreply.github.com>
2022-01-28 16:19:44 +0000

    tbpi1 quase completo site html

--- 2ano/code/trabalho_final/funcoes.h
+++
@@ -1,7 +0,0 @@
-extern void readfromfileinsertme(ME array[]);
-extern int insertme(ME array[],int index, int ordem, int nif, char *codigo, int tempo, int distancia);
-//extern int existeme(ME array[], int index, int ordem);
-extern void printarray(ME array[], int i);
-extern int menu();
-extern void printartudome(ME array[]);
-extern void readfromfileinsertmu(MU array[]);
